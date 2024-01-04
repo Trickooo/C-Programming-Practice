059d7070a164c23c6b4bb226f733e10a7e4bebed
John Patrick Gonzales <Johnpatrickgonzales2608@gmail.com>
2024-01-04 18:09:25 +0800

    Update Function.cpp

--- Function.cpp
+++ Function.cpp
@@ -6,7 +6,10 @@ void myFunction(string name,
 				string month,
 				int year,
 				int age,
-				int day = 99){ //we placed our defaults at the last part in order for function to work, the problem is that our parameters are not at the end for both cases as the previous parameter value cannot be empty before another like the case for patrick and princess. princess' default is at last parameter and patrick's was before the last parameter, Patrick's cannot be empty and we couldn't really use the same function for this because the output would be wrong.
+				int day = 99){ 		//we placed our defaults at the last part in order for function to work, the problem is that our 
+									//parameters are not at the end for both cases as the previous parameter value cannot be empty before another like the case for patrick 
+									//and princess. princess' default is at last parameter and patrick's was before the last parameter, 
+									//Patrick's cannot be empty and we couldn't really use the same function for this because the output would be wrong.
 					cout << "My name is " << name << ", age is " << age << ", My birthday is " << month << " " << day << " " << year << endl;
 				}
 				
@@ -15,7 +18,9 @@ void specialFunction(string name,
 					int year,
 					int day,
 					int age = 100){	
-					// I created another function in order for our default for Patrick to work because in one of C++ default constraints is that the default should always be the last parameter, if not, the parameter will ask for default values for the next parameter values.
+									// I created another function in order for our default for Patrick to work 
+									//because in one of C++ default constraints is that the default should always be the last parameter, 
+									//if not, the parameter will ask for default values for the next parameter values.
 					cout << "My name is " << name << ", age is " << age << ", My birthday is " << month << " " << day << " " << year << endl;
 				}
 int main(int argc, char** argv) {
